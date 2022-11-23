ce208ddb52ef8a0be1f38ec1b4a15d0773c96c77
Misha Behersky <bmwant@gmail.com>
2022-11-23 13:20:21 +0000

    chore(mappi-1): Dynamically attach routes on app :earth_americas:

--- mappi/__main__.py
+++ mappi/__main__.py
@@ -1,9 +1,22 @@
 import uvicorn
 
-from mappi.main import app
+
+from fastapi import FastAPI, APIRouter
 
 
+def _create_routes():
+    router = APIRouter()
+    return router
+
+
+def _create_app():
+    app = FastAPI()
+    index = lambda: {"Hello": "World"}
+    app.router.add_api_route("/", index)
+    return app
+    
 def run():
+    app = _create_app()
     config = uvicorn.Config(app, port=5000, log_level="info")
     server = uvicorn.Server(config)
     server.run()
