4879ca706d4391fad69f1f46f469c2c8bbc48bd9
Misha Behersky <bmwant@gmail.com>
2022-11-23 18:26:37 +0000

    Add loading yaml configuration files :snake:

--- mappi/__main__.py
+++ mappi/__main__.py
@@ -1,7 +1,12 @@
-import uvicorn
+from pathlib import Path
 
+import uvicorn
+import yaml
 
 from fastapi import FastAPI, APIRouter
+
+
+CURRENT_DIR = Path(__file__).parent.resolve()
 
 
 def _create_routes():
@@ -15,7 +20,20 @@ def _create_app():
     app.router.add_api_route("/", index)
     return app
     
+
+def read_configuration():
+    filename = CURRENT_DIR / "mappi.yml"
+    routes = []
+    with open(filename) as f:
+        conf = yaml.load(f.read(), Loader=yaml.FullLoader)
+        routes = conf["routes"]
+
+    for route in routes:
+        print(route["path"])
+        
+        
 def run():
+    read_configuration()
     app = _create_app()
     config = uvicorn.Config(app, port=5000, log_level="info")
     server = uvicorn.Server(config)
