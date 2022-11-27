0c26f7de09c3732614b681166f513879dc11f906
Misha Behersky <bmwant@gmail.com>
2022-11-27 14:57:45 +0000

    Add server version header to the response :chart_with_upwards_trend:

--- mappi/server.py
+++ mappi/server.py
@@ -1,23 +1,23 @@
 from typing import List
 
+import pkg_resources
 from fastapi import FastAPI, Request
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from mappi import schema
 from mappi.handlers import handler_factory
+from mappi.utils import logger
 
 
 class MappiMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         response = await call_next(request)
-        response.headers["server"] = "mappi"
+
+        version = pkg_resources.get_distribution(__package__).version
+        server_header = f"{__package__} {version}"
+        logger.debug(f"Adding header {server_header}")
+        response.headers["server"] = server_header
         return response
-
-
-async def add_server_header(request: Request, call_next):
-    response = await call_next(request)
-
-    return response
 
 
 def create_app(routes: List[schema.Route]):
