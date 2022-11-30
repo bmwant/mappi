6d0492b49342b9a633f770fd089b3b4be5ffbba9
Misha Behersky <bmwant@gmail.com>
2022-11-30 18:11:14 +0000

    Logging config for default mappi logger :bowtie:

--- mappi/utils.py
+++ mappi/utils.py
@@ -5,19 +5,6 @@ import yaml
 
 from mappi import config, schema
 
-# logging.disable(level=logging.CRITICAL)
-
-# unicorn_access_logger = logging.getLogger("uvicorn.access")
-# unicorn_error_logger = logging.getLogger("uvicorn.error")
-# unicorn_access_logger.disabled = True
-# unicorn_error_logger.disabled = True
-
-# if config.DEBUG:
-# if True:
-#     logging.disable(logging.NOTSET)
-#     logging.basicConfig(level=logging.DEBUG)
-
-
 logging.config.dictConfig(config.LOGGING_CONFIG)
 logger = logging.getLogger(__package__)
 
