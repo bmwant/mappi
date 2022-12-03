a5119b6e975dcfcb18e63b734d0710d7aef8ba3c
Misha Behersky <bmwant@gmail.com>
2022-12-03 14:53:09 +0000

    fix: Remove unused unittests :hammer:

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -8,14 +8,3 @@
 # @pytest.mark.parametrize("make_config", ["status_500.yml"])
 def test_parametrize():
     pass
-
-
-def my_get(url, params=None):
-    print("This is url", url)
-
-
-def test_free_port(free_port):
-    from tests.utils import update_url
-
-    f = update_url(my_get, "http://localhost:54322")
-    f("/path")
