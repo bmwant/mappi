f1f5553e6ed84f7e851366c63441d4b9b633c67d
Misha Behersky <bmwant@gmail.com>
2022-12-04 16:33:39 +0000

    feat: Just testing version bump

--- tests/integration/test_text_response.py
+++ tests/integration/test_text_response.py
@@ -5,3 +5,12 @@ import pytest
 from tests.utils import use_config  # noqa: F401
 
 pytestmark = pytest.mark.integration
+
+
+@use_config("status_5xx.yml")
+def test_text_response(test_server, test_client):
+    response = test_client.get("/test_500")
+
+    assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
+    assert "Internal Server Error" in response.text
