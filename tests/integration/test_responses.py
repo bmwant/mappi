34f3c85ac66e5f1971e2e795ad743d63fcb1b913
Misha Behersky <bmwant@gmail.com>
2022-12-07 16:16:50 +0000

    test: add tests for wrong method/path :point_left:

---
+++ tests/integration/test_responses.py
@@ -0,0 +1,33 @@
+from http import HTTPStatus  # noqa: F401
+
+import pytest
+
+from tests.utils import use_config  # noqa: F401
+
+pytestmark = pytest.mark.integration
+
+
+@use_config("test_default.yml")
+def test_base_response(test_server, test_client):
+    response = test_client.get("/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers["content-type"] == "text/plain; charset=utf-8"
+    assert response.text == "Simple test response"
+
+
+@use_config("test_default.yml")
+def test_not_found_response(test_server, test_client):
+    response = test_client.get("/test_not_found")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.headers["content-type"] == "text/plain; charset=utf-8"
+    assert response.text == "No mapping registered for /test_not_found route"
+
+
+@use_config("test_default.yml")
+def test_wrong_method(test_server, test_client):
+    response = test_client.post("/")
+
+    assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+    assert response.text == "Method Not Allowed"
